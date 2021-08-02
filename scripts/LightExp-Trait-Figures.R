72556d46d5f710c58b4a7ddd0c4929dc46ee82e5
Taylor Stewart <taylor.stewart@uvm.edu>
2021-08-02 10:43:15 -0400

    adjust figures for B&W printing

--- scripts/LightExp-Trait-Figures.R
+++ scripts/LightExp-Trait-Figures.R
@@ -24,12 +24,13 @@ traitsStand.all <- bind_rows(hatch.survival.summary.stand, hatch.dpf.summary.sta
 #### VISUALIZATIONS - MEANS ----------------------------------------------------------------------
 
 ## Embryo Survival
-plot.survival <- ggplot(filter(traitsOverall.all, trait == "survival"), aes(x = population, y = (mean.trait*100), group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.survival <- ggplot(filter(traitsOverall.all, trait == "survival"), aes(x = population, y = (mean.trait*100), group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_errorbar(aes(ymin = (mean.trait - se.trait) * 100, ymax = (mean.trait + se.trait) * 100), 
                 position = position_dodge(0.6),
                 size = 1, width = 0.25, linetype = "solid", show.legend = FALSE) +
   scale_y_continuous(limits = c(80, 101), breaks = seq(80, 100, 5), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Mean Embryo Survival (%)", x = "Population") +
@@ -46,12 +47,13 @@ plot.survival <- ggplot(filter(traitsOverall.all, trait == "survival"), aes(x = 
         plot.margin = unit(c(5, 5, 5, 5), 'mm'))
 
 ## Days Post Fertilization
-plot.dpf <- ggplot(filter(traitsOverall.all, trait == "dpf"), aes(x = population, y = mean.trait, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.dpf <- ggplot(filter(traitsOverall.all, trait == "dpf"), aes(x = population, y = mean.trait, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_errorbar(aes(ymin = mean.trait - se.trait, ymax = mean.trait + se.trait), 
                 position = position_dodge(0.6),
                 size = 1, width = 0.25, linetype = "solid", show.legend = FALSE) +
   scale_y_continuous(limits = c(95, 120), breaks = seq(95, 130, 5), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Mean DPF", x = "Population") +
@@ -68,12 +70,13 @@ plot.dpf <- ggplot(filter(traitsOverall.all, trait == "dpf"), aes(x = population
         plot.margin = unit(c(5, 5, 5, 5), 'mm'))
 
 ## Accumulated Degree-Days
-plot.add <- ggplot(filter(traitsOverall.all, trait == "ADD"), aes(x = population, y = mean.trait, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.add <- ggplot(filter(traitsOverall.all, trait == "ADD"), aes(x = population, y = mean.trait, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_errorbar(aes(ymin = mean.trait - se.trait, ymax = mean.trait + se.trait), 
                 position = position_dodge(0.6),
                 size = 1, width = 0.25, linetype = "solid", show.legend = FALSE) +
   scale_y_continuous(limits = c(400, 505), breaks = seq(350, 500, 25), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Mean ADD (°C)", x = "Population") +
@@ -90,12 +93,13 @@ plot.add <- ggplot(filter(traitsOverall.all, trait == "ADD"), aes(x = population
         plot.margin = unit(c(5, 5, 5, 5), 'mm'))
 
 ## Length-at-Hatch
-plot.tl <- ggplot(filter(traitsOverall.all, trait == "LAH"), aes(x = population, y = mean.trait, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.tl <- ggplot(filter(traitsOverall.all, trait == "LAH"), aes(x = population, y = mean.trait, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_errorbar(aes(ymin = mean.trait - se.trait, ymax = mean.trait + se.trait), 
                 position = position_dodge(0.6),
                 size = 1, width = 0.25, linetype = "solid", show.legend = FALSE) +
   scale_y_continuous(limits = c(9.5, 11), breaks = seq(9.5, 11, 0.5), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Mean LAH (mm)", x = "Population") +
@@ -112,12 +116,13 @@ plot.tl <- ggplot(filter(traitsOverall.all, trait == "LAH"), aes(x = population,
         plot.margin = unit(c(5, 5, 5, 5), 'mm'))
 
 ## Yolk-sac Volume
-plot.ysv <- ggplot(filter(traitsOverall.all, trait == "YSV"), aes(x = population, y = mean.trait, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.ysv <- ggplot(filter(traitsOverall.all, trait == "YSV"), aes(x = population, y = mean.trait, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_errorbar(aes(ymin = mean.trait - se.trait, ymax = mean.trait + se.trait), 
                 position = position_dodge(0.6),
                 size = 1, width = 0.25, linetype = "solid", show.legend = FALSE) +
   scale_y_continuous(limits = c(0.3, 0.7), breaks = seq(0.3, 0.7, 0.1), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = expression("Mean YSV (mm"^3*")"), x = "Population") +
@@ -152,18 +157,19 @@ plot.overall.all <- grid.arrange(
   heights = c(0.15, 1.0, 0.05, 1.0, 0.05, 0.04)
 )
 
-ggsave("figures/Fig3.tiff", plot = plot.overall.all, width = 14, height = 11, dpi = 300)
+ggsave("figures/Fig3.tiff", plot = plot.overall.all, width = 14, height = 11, dpi = 250)
 
 
 #### VISUALIZATIONS - STANDARDIZED ---------------------------------------------------------------
 
 ## Plot Standardized Survival
-plot.survival.stand <- ggplot(filter(traitsStand.all, trait == "survival"), aes(x = population, y = mean.trait.stand, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.survival.stand <- ggplot(filter(traitsStand.all, trait == "survival"), aes(x = population, y = mean.trait.stand, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_hline(yintercept = 0, color = "gray30", linetype = "dashed", alpha = 0.5) +
   geom_errorbar(aes(ymin = (mean.trait.stand - se.trait.stand), ymax = (mean.trait.stand + se.trait.stand)), 
                 position = position_dodge(0.6), size = 0.8, width = 0.4, show.legend = FALSE) +
   scale_y_continuous(limits = c(-20.0, 20), breaks = seq(-20.0, 20, 10), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Standardized Embryo Survival (%)", x = "Population") +
@@ -180,12 +186,13 @@ plot.survival.stand <- ggplot(filter(traitsStand.all, trait == "survival"), aes(
         plot.margin = unit(c(5, 5, 5, 5), 'mm')) 
 
 ## Plot Standardized DPF
-plot.dpf.stand <- ggplot(filter(traitsStand.all, trait == "dpf"), aes(x = population, y = mean.trait.stand, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.dpf.stand <- ggplot(filter(traitsStand.all, trait == "dpf"), aes(x = population, y = mean.trait.stand, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_hline(yintercept = 0, color = "gray30", linetype = "dashed", alpha = 0.5) +
   geom_errorbar(aes(ymin = (mean.trait.stand - se.trait.stand), ymax = (mean.trait.stand + se.trait.stand)), 
                 position = position_dodge(0.6), size = 0.8, width = 0.4, show.legend = FALSE) +
   scale_y_continuous(limits = c(-20.0, 20), breaks = seq(-20.0, 20, 10), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Standardized DPF (%)", x = "Population") +
@@ -202,12 +209,13 @@ plot.dpf.stand <- ggplot(filter(traitsStand.all, trait == "dpf"), aes(x = popula
         plot.margin = unit(c(5, 5, 5, 5), 'mm')) 
 
 ## Plot Standardized ADD
-plot.add.stand <- ggplot(filter(traitsStand.all, trait == "ADD"), aes(x = population, y = mean.trait.stand, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.add.stand <- ggplot(filter(traitsStand.all, trait == "ADD"), aes(x = population, y = mean.trait.stand, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_hline(yintercept = 0, color = "gray30", linetype = "dashed", alpha = 0.5) +
   geom_errorbar(aes(ymin = (mean.trait.stand - se.trait.stand), ymax = (mean.trait.stand + se.trait.stand)), 
                 position = position_dodge(0.6), size = 0.8, width = 0.4, show.legend = FALSE) +
   scale_y_continuous(limits = c(-20.0, 20), breaks = seq(-20.0, 20, 10), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Standardized ADD (%)", x = "Population") +
@@ -224,12 +232,13 @@ plot.add.stand <- ggplot(filter(traitsStand.all, trait == "ADD"), aes(x = popula
         plot.margin = unit(c(5, 5, 5, 5), 'mm')) 
 
 ## Plot Standardized LAH
-plot.tl.stand <- ggplot(filter(traitsStand.all, trait == "LAH"), aes(x = population, y = mean.trait.stand, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.tl.stand <- ggplot(filter(traitsStand.all, trait == "LAH"), aes(x = population, y = mean.trait.stand, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_hline(yintercept = 0, color = "gray30", linetype = "dashed", alpha = 0.5) +
   geom_errorbar(aes(ymin = (mean.trait.stand - se.trait.stand), ymax = (mean.trait.stand + se.trait.stand)), 
                 position = position_dodge(0.6), size = 0.8, width = 0.4, show.legend = FALSE) +
   scale_y_continuous(limits = c(-20.0, 20), breaks = seq(-20.0, 20, 10), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Standardized LAH (%)", x = "Population") +
@@ -246,12 +255,13 @@ plot.tl.stand <- ggplot(filter(traitsStand.all, trait == "LAH"), aes(x = populat
         plot.margin = unit(c(5, 5, 5, 5), 'mm')) 
 
 ## Plot Standardized YSV
-plot.ysv.stand <- ggplot(filter(traitsStand.all, trait == "YSV"), aes(x = population, y = mean.trait.stand, group = light, color = light)) + 
-  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5, shape = 1) +
+plot.ysv.stand <- ggplot(filter(traitsStand.all, trait == "YSV"), aes(x = population, y = mean.trait.stand, group = light, color = light, shape = light)) + 
+  geom_point(size = 5, position = position_dodge(0.6), stroke = 1.5) +
   geom_hline(yintercept = 0, color = "gray30", linetype = "dashed", alpha = 0.5) +
   geom_errorbar(aes(ymin = (mean.trait.stand - se.trait.stand), ymax = (mean.trait.stand + se.trait.stand)), 
                 position = position_dodge(0.6), size = 0.8, width = 0.4, show.legend = FALSE) +
   scale_y_continuous(limits = c(-20.0, 20), breaks = seq(-20.0, 20, 10), expand = c(0, 0)) +
+  scale_shape_manual("combine", values = c(21, 22, 24), labels = c("Low ", "Medium ", "High")) +
   scale_color_manual("combine", values = c("#0571b0", "#92c5de", "#f4a582"),
                      labels = c("Low ", "Medium ", "High")) +
   labs(y = "Standardized YSV (%)", x = "Population") +
@@ -286,5 +296,5 @@ plot.stand.all <- grid.arrange(
   heights = c(0.15, 1.0, 0.05, 1.0, 0.05, 0.04)
 )
 
-ggsave("figures/Fig4.tiff", plot = plot.stand.all, width = 14, height = 11, dpi = 300)
+ggsave("figures/Fig4.tiff", plot = plot.stand.all, width = 14, height = 11, dpi = 250)
 
